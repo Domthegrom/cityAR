48247d2c7745a8e9b2e32e001b086fe707bb1b20
Dominic <dgozza@peakactivity.com>
2020-02-14 15:36:05 -0500

    title test

--- src/ModelViewer.tsx
+++ src/ModelViewer.tsx
@@ -1,35 +1,21 @@
 import React, { useEffect } from 'react'
 import '@google/model-viewer';
 
-
-
-// src="https://cdn.glitch.com/21710700-a398-4f93-9a6c-eb0810904e74/industrial_lamp.gltf?v=1569485480008"
-// ios-src="https://cdn.glitch.com/21710700-a398-4f93-9a6c-eb0810904e74/industrial_floor_lamp.usdz?v=1569485305204"
-// alt="Tall Lamp" background-color="#eeeeee" shadow-intensity="1" camera-controls ar magic-leap
-// quick-look-browsers="safari chrome" ar-scale="fixed" resizable="false"
-// max-field-of-view="90deg"
-
-
-
-
-
-
 export function ModelViewer() {
     useEffect(() => {
         const model = document.createElement('model-viewer');
         model.src = "https://cdn.glitch.com/21710700-a398-4f93-9a6c-eb0810904e74/industrial_lamp.gltf?v=1569485480008";
         model.ar = true
         model.autoRotate = true
-        model.backgroundColor = '#000000'
         model.arScale = 'fixed'
         model.iosSrc = "https://cdn.glitch.com/21710700-a398-4f93-9a6c-eb0810904e74/industrial_floor_lamp.usdz?v=1569485305204"
+        model.maxFieldOfView = '20deg'
+        model.title = "Some Lamp"
+        model.toDataURL = "https://www.cityfurniture.com/product/5450017/tripod-metal-table-lamp"
         const modelDiv = document.getElementById('model')
         modelDiv?.appendChild(model)
     }, [])
     return (
-        <div id="model">
-
-        </div>
-
+        <div id="model"></div>
     )
 }
